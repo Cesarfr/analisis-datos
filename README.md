4089434b896e34aa787bed90196da71b4a864fe3
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2016-04-10 02:42:41 -0500

    Corregidos los siguientes aspectos: 	* Grafico de barras cuando los valores sean mayores a 6 	* Aumentado el tamaño de los CSV 	* Corregida la renderizacion de los calculos estadisticos y agregados curtosis, rango y max-min

--- README.md
+++ README.md
@@ -12,13 +12,27 @@ Esta aplicación usa el lenguaje de programación R, el paquete Shiny para crear
 * RMySQL
 * agricolae
 * plotrix
+* knitr
+* rmarkdown
 
 ## Instalacion de los paquetes necesarios
 
 Para instalar los paquetes necesarios ejecute este comando en la consola de R:
 
-`install.packages(c("shiny", "shinyjs", "shinyBS", "modeest", "DT","RMySQL", "agricolae", "plotrix"))`
+`install.packages(c("shiny", "shinyjs", "shinyBS", "modeest", "DT","RMySQL", "agricolae", "plotrix", "knitr", "rmarkdown"))`
 
 ## Métodos de carga de datos
 
 Para trabajar, la aplicación puede manejar datos provenientes de archivos **CSV**, ingreso **manual** de los datos separados por comas y conexión a una base de datos.
+
+## Creación de reportes
+
+La aplicación puede generar reportes en los formatos siguientes:
+
+* PDF
+* Word
+* HTML
+
+Para la creación de reportes en PDF es necesario tener instalado TeXLive para ello en Ubuntu puede instalarlo ejecutando el siguiente comando
+
+` sudo apt-get install texlive texlive-latex-extra`
--- server.R
+++ server.R
@@ -10,6 +10,8 @@ library(agricolae)
 library(knitr)
 library(rmarkdown)
 
+options(shiny.maxRequestSize=50*1024^2) 
+
 shinyServer(function(input, output, session) {
   inFile <- NULL
   datoscsv <- NULL
@@ -20,9 +22,25 @@ shinyServer(function(input, output, session) {
   tableSelected <- NULL
   tipoDatos <- NULL
   gr <- NULL
+  noLevels <- NULL
   valores <- NULL
   porcent <- NULL
   fecha <- NULL
+  media <- NULL
+  mediana <- NULL
+  moda <- NULL
+  qu1 <- NULL
+  qu2 <- NULL
+  qu3 <- NULL
+  pe10 <- NULL
+  pe50 <- NULL
+  pe90 <- NULL
+  vari <- NULL
+  desvest <- NULL
+  sesg <- NULL
+  rang <- NULL
+  curt <- NULL
+  mamin <- NULL
   
   output$cargaDatos <- renderUI({
     
@@ -139,6 +157,12 @@ shinyServer(function(input, output, session) {
       rs <- dbSendQuery(conexionDB, paste0("SELECT * FROM ", tableSelected))
       datos <- dbFetch(rs, n = -1)
       dbClearResult(rs)
+      nm <- names(datos)
+      for(i in 1:length(datos)){
+        if(class(unlist(datos[i])) == "character"){
+          datos[[nm[i]]] <- as.factor(datos[[nm[i]]])
+        }
+      }
       datosObt <- data.frame(datos)
       return(datosObt)
     })
@@ -213,6 +237,11 @@ shinyServer(function(input, output, session) {
                    downloadButton('downloadPlot', label = "Descargar gráfico"),
                    plotOutput("graf")
             )
+          ),
+          fluidRow(
+            column(
+                   12, "  "
+            )
           )
         )
       )
@@ -235,15 +264,23 @@ shinyServer(function(input, output, session) {
       legend(x = "topright", c("Media", "Moda", "Mediana"), col = c("red", "#37CF11", "blue"), lwd = c(2, 2, 2), bty = "n")
     }else{
       # Datos cualitativos
+      noLevels <<- length(levels(datoscsv[[grf]]))
       valores <<- table(datoscsv[[grf]])
-      lbls <- names(valores)
-      porcent <<- round(valores/sum(valores)*100)
-      lbls <- paste(lbls, porcent)
-      lbls <- paste(lbls, "%", sep = "")
-      pie3D(
-        valores, labels = lbls, main = paste("Gráfica de pastel de", grf), radius = 1, labelrad = 1.5, explode = 0.1, 
-        col = rainbow(length(valores)), shade = 0.7, theta = 0.9, start = 3
-      )
+      if(noLevels <= 6){
+        lbls <- names(valores)
+        lbls <- names(valores)
+        porcent <<- round(valores/sum(valores)*100)
+        lbls <- paste(lbls, porcent)
+        lbls <- paste(lbls, "%", sep = "")
+        pie3D(
+          valores, labels = lbls, main = paste("Gráfica de pastel de", grf), radius = 1, labelrad = 1.5, explode = 0.1, 
+          col = rainbow(length(valores)), shade = 0.7, theta = 0.9, start = 3
+        )
+      }else if(noLevels > 6 && noLevels <=50){
+        barplot(valores, col = rainbow(length(valores)), main = paste("Gráfica de barras de", grf), xlab = "Valor", ylab = "Frecuencia", las=2)
+      }else{
+        barplot(valores, col = rainbow(length(valores)), main = paste("Gráfica de barras de", grf), xlab = "Valor", ylab = "Frecuencia")
+      }
     }
     re()
   })
@@ -251,91 +288,98 @@ shinyServer(function(input, output, session) {
   # Cambio de valores segun lo elegido en el grafico
   re <- eventReactive(input$selPlot, ({
     if(tipoDatos == "integer" || tipoDatos == "numeric"){
-      output$media <- renderText({
-        round(mean(as.double(datoscsv[[grf]])), digits = 2)
-      })
-      output$mediana <- renderText({
-        round(median(as.double(datoscsv[[grf]])), digits = 2)
-      })
-      output$moda <- renderText({
-        paste(round(mfv(as.double(datoscsv[[grf]])), digits = 2), collapse = ", ")
-      })
-      output$q1 <- renderText({
-        round(quantile(as.double(datoscsv[[grf]]), .25), digits = 2)
-      })
-      output$q2 <- renderText({
-        round(quantile(as.double(datoscsv[[grf]]), .50), digits = 2)
-      })
-      output$q3 <- renderText({
-        round(quantile(as.double(datoscsv[[grf]]), .75), digits = 2)
-      })
-      output$p10 <- renderText({
-        round(quantile(as.double(datoscsv[[grf]]), .10), digits = 2)
-      })
-      output$p50 <- renderText({
-        round(quantile(as.double(datoscsv[[grf]]), .50), digits = 2)
-      })
-      output$p90 <- renderText({
-        round(quantile(as.double(datoscsv[[grf]]), .90), digits = 2)
-      })
-      output$varianza <- renderText({
-        round(var(as.double(datoscsv[[grf]])), digits = 5)
-      })
-      output$desest <- renderText({
-        round(sd(as.double(datoscsv[[grf]])), digits = 5)
-      })
-      output$sesgo <- renderText({
-        round(skewness(as.double(datoscsv[[grf]])), digits = 5)
-      })
+      media <<- round(mean(as.double(datoscsv[[grf]])), digits = 2)
+      mediana <<- round(median(as.double(datoscsv[[grf]])), digits = 2)
+      moda <<- paste(round(mfv(as.double(datoscsv[[grf]])), digits = 2), collapse = ", ")
+      qu1 <<- round(quantile(as.double(datoscsv[[grf]]), .25), digits = 2)
+      qu2 <<- round(quantile(as.double(datoscsv[[grf]]), .50), digits = 2)
+      qu3 <<- round(quantile(as.double(datoscsv[[grf]]), .75), digits = 2)
+      pe10 <<- round(quantile(as.double(datoscsv[[grf]]), .10), digits = 2)
+      pe50 <<- round(quantile(as.double(datoscsv[[grf]]), .50), digits = 2)
+      pe90 <<- round(quantile(as.double(datoscsv[[grf]]), .90), digits = 2)
+      vari <<- round(var(as.double(datoscsv[[grf]])), digits = 5)
+      desvest <<- round(sd(as.double(datoscsv[[grf]])), digits = 5)
+      sesg <<- round(skewness(as.double(datoscsv[[grf]])), digits = 5)
+      maxi <<- max(as.double(datoscsv[[grf]]))
+      minim <<- min(as.double(datoscsv[[grf]]))
+      mamin <<- paste(maxi, minim, sep = " - ")
+      rang <<- round((maxi - minim), digits = 2)
+      curt <<- round(kurtosis(as.double(datoscsv[[grf]])), digits = 5)
       output$tbDatAg <- DT::renderDataTable(({
         tbf <- table.freq(gr)
         colnames(tbf) <- c("LInf", "LSup", "MClase", "Frecuencia", "Porc", "FrecAcum", "PorcAcum")
         tbf
       }), class = "cell-border stripe", extensions = "Responsive")
     }else{
-      output$media <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$mediana <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$moda <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$q1 <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$q2 <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$q3 <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$p10 <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$p50 <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$p90 <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$varianza <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$desest <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
-      output$sesgo <- renderText({
-        "No es posible calcular para datos cualitativos"
-      })
+      media <- "No es posible calcular para datos cualitativos"
+      mediana <- "No es posible calcular para datos cualitativos"
+      moda <- "No es posible calcular para datos cualitativos"
+      qu1 <- "No es posible calcular para datos cualitativos"
+      qu2 <- "No es posible calcular para datos cualitativos"
+      qu3 <- "No es posible calcular para datos cualitativos"
+      pe10 <- "No es posible calcular para datos cualitativos"
+      pe50 <- "No es posible calcular para datos cualitativos"
+      pe90 <- "No es posible calcular para datos cualitativos"
+      vari <- "No es posible calcular para datos cualitativos"
+      desvest <- "No es posible calcular para datos cualitativos"
+      sesg <- "No es posible calcular para datos cualitativos"
+      rang <- "No es posible calcular para datos cualitativos"
+      curt <- "No es posible calcular para datos cualitativos"
+      mamin <- "No es posible calcular para datos cualitativos"
       output$tbDatAg <- DT::renderDataTable(({
         dtf <- data.frame(valores)
         colnames(dtf) <- c("Valores", "Frecuencia")
-        dtf$Porcentaje <- porcent
+        if(!is.null(noLevels) && noLevels <= 6){
+          dtf$Porcentaje <- porcent 
+        }
         dtf
       }), class = "cell-border stripe", extensions = "Responsive")
     }
+    output$media <- renderText({
+      media
+    })
+    output$mediana <- renderText({
+      mediana
+    })
+    output$moda <- renderText({
+      moda
+    })
+    output$q1 <- renderText({
+      qu1
+    })
+    output$q2 <- renderText({
+      qu2
+    })
+    output$q3 <- renderText({
+      qu3
+    })
+    output$p10 <- renderText({
+      pe10
+    })
+    output$p50 <- renderText({
+      pe50
+    })
+    output$p90 <- renderText({
+      pe90
+    })
+    output$varianza <- renderText({
+      vari
+    })
+    output$desest <- renderText({
+      desvest
+    })
+    output$sesgo <- renderText({
+      sesg
+    })
+    output$rango <- renderText({
+      rang
+    })
+    output$curtosis <- renderText({
+      curt
+    })
+    output$mn <- renderText({
+      mamin
+    })
   }))
   
   # Calculos estadisticos
@@ -418,7 +462,7 @@ shinyServer(function(input, output, session) {
       ),
       fluidRow(
         column(
-          12, h3("Otros datos")
+          12, h3("Otros cálculos")
         )
       ),
       fluidRow(
@@ -438,6 +482,26 @@ shinyServer(function(input, output, session) {
           4, tags$div(class="panel panel-warning",
                       tags$div(class="panel-heading", tags$h4(class="panel-title"), "Sesgo"),
                       tags$div(class="panel-body", textOutput("sesgo"))
+          )
+        )
+      ),
+      fluidRow(
+        column(
+          4, tags$div(class="panel panel-warning",
+                      tags$div(class="panel-heading", tags$h4(class="panel-title"), "Curtosis"),
+                      tags$div(class="panel-body", textOutput("curtosis"))
+          )
+        ),
+        column(
+          4, tags$div(class="panel panel-warning",
+                      tags$div(class="panel-heading", tags$h4(class="panel-title"), "Rango"),
+                      tags$div(class="panel-body", textOutput("rango"))
+          )
+        ),
+        column(
+          4, tags$div(class="panel panel-warning",
+                      tags$div(class="panel-heading", tags$h4(class="panel-title"), "Máximo, Mínimo"),
+                      tags$div(class="panel-body", textOutput("mn"))
           )
         )
       )
@@ -497,18 +561,29 @@ shinyServer(function(input, output, session) {
       }else{
         # Datos cualitativos
         valores <<- table(datoscsv[[grf]])
-        lbls <- names(valores)
-        porcent <<- round(valores/sum(valores)*100)
-        lbls <- paste(lbls, porcent)
-        lbls <- paste(lbls, "%", sep = "")
-        pie3D(
-          valores, labels = lbls, main = paste("Gráfica de pastel de", grf), explode = 0.1, 
-          col = rainbow(length(valores)), theta = 0.9
-        )
+        if(noLevels <= 6){
+          lbls <- names(valores)
+          porcent <<- round(valores/sum(valores)*100)
+          lbls <- paste(lbls, porcent)
+          lbls <- paste(lbls, "%", sep = "")
+          pie3D(
+            valores, labels = lbls, main = paste("Gráfica de pastel de", grf), radius = 1, labelrad = 1.5, explode = 0.1, 
+            col = rainbow(length(valores)), shade = 0.7, theta = 0.9, start = 3
+          )
+        }else{
+          barplot(valores, col = rainbow(length(valores)), main = paste("Gráfica de pastel de", grf), xlab = "Valor", ylab = "Frecuencia")
+        }
         dev.off()
       }
     },
     contentType = 'image/png'
   )
+  
+  # Cerrar conexion DB
+  session$onSessionEnded(function() {
+    observe(
+      dbDisconnect(conn = conexionDB)
+    )
+  })
   
 })
